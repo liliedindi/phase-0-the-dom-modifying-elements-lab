de91c4d48fff7d6d3433261b74ee4421f241948d
liliedindi <liliandindinabwire@gmail.com>
2024-07-25 07:30:42 +0300

    dom nodes

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const main =document.querySelector('main#main')
+main.remove();
+const newHeader=document.createElement('h1')
+document.body.append(newHeader)
+newHeader.nodeName=('H1')
+newHeader.id=("victory");
+newHeader.innerHTML =('lils is the champion')
